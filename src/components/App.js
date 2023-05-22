2f3c0ab09f1663992bbefb358d225f39ee951597
samuel <samuelkelvinmwangi@gmail.com>
2023-05-22 15:53:36 +0300

    adding components and children

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,74 @@ import React from "react";
 import blogData from "../data/blog";
 
 console.log(blogData);
+//prop function ;
+function Header(props) {
+  const { blogName } = props;
 
+  return (
+    <header>
+      <h1>{blogName}</h1>
+    </header>
+  );
+}
+
+function About(props) {
+  const { imageSrc, aboutText } = props;
+  const placeholderImage = "https://via.placeholder.com/215";
+
+  return (
+    <aside>
+      <img src={imageSrc || placeholderImage} alt="blog logo" />
+      <p>{aboutText}</p>
+    </aside>
+  );
+}
+
+
+function Article(props) {
+  const { title, date, preview } = props;
+  const defaultDate = "December 15, 2020";
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date || defaultDate}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+function ArticleList(props) {
+  const { posts } = props;
+
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article
+          key={index}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+
+
+//displaying in app;
 function App() {
+
+  const blogName = "Overreacted";
+  const imageSrc = "";
+  const aboutText = "Personal Blog By Samuel Mwangi";
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName={blogName} />
+      <About imageSrc={imageSrc} aboutText={aboutText} />
+      <ArticleList posts={posts} />
+      
     </div>
   );
 }
